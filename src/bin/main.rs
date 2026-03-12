b0388e6b8b88228ee37f28afbac7ef1d0b8e0799
Friso Smit <fw.smit01@gmail.com>
2026-03-12 17:45:44 +0700

    Remove some commented code

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -12,24 +12,13 @@ type Backend = NdArray<f32>;
 // Get the backend device we use (cpu)
 type BackendDevice = <Backend as burn::tensor::backend::Backend>::Device;
 
+use burn::{backend::NdArray, tensor::Tensor};
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
 use esp_hal::main;
 use esp_hal::time::{Duration, Instant};
 use log::info;
-use burn::{backend::NdArray, tensor::Tensor};
-// use embassy_executor::Spawner;
-// use embassy_rp::{
-//     bind_interrupts,
-//     gpio::{Level, Output},
-//     peripherals::USB,
-//     usb::{Driver, InterruptHandler},
-// };
-// use embassy_time::Timer;
-// use embedded_alloc::LlffHeap as Heap;
 use mamba_embedded::sine::Model;
-// use {defmt_rtt as _, panic_probe as _};
-
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
@@ -84,37 +73,6 @@ fn main() -> ! {
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples
 }
 
-// #[global_allocator]
-// static HEAP: Heap = Heap::empty();
-//
-// bind_interrupts!(struct Irqs {
-//     USBCTRL_IRQ => InterruptHandler<USB>;
-// });
-
-// This is the main function for the program. Where execution starts.
-// #[embassy_executor::main]
-// async fn main(spawner: Spawner) {
-    // Initializes the allocator, must be done before use.
-    // {
-        // use core::mem::MaybeUninit;
-        // // Watch out for this, if it is too big or small, program may crash
-        // // this is in u8 bytes, as such this is a total of 100kb
-        // const HEAP_SIZE: usize = 100 * 1024;
-        // static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
-        // unsafe { HEAP.init(&raw mut HEAP_MEM as usize, HEAP_SIZE) } // Initialize the heap
-    // }
-
-    // This is just setup to make the microcontroller output to serial.
-    // let p = embassy_rp::init(Default::default());
-    // let driver = Driver::new(p.USB, Irqs);
-    // spawner.spawn(logger_task(driver)).unwrap();
-    //
-    // // Set the onboard LED to high to help indicate that the program is working properly.
-    // let mut led = Output::new(p.PIN_25, Level::Low);
-    // led.set_high();
-
-// }
-
 fn run_model(model: &Model<NdArray>, device: &BackendDevice, input: f32) -> Tensor<Backend, 2> {
     // Define the tensor
     let input = Tensor::<Backend, 2>::from_floats([[input]], device);
@@ -122,10 +80,3 @@ fn run_model(model: &Model<NdArray>, device: &BackendDevice, input: f32) -> Tens
     // Run the model on the input
     model.forward(input)
 }
-
-// This runs as a separate task whenever there is an await
-// #[embassy_executor::task]
-// async fn logger_task(driver: Driver<'static, USB>) {
-//     // This just makes a logger that outputs to serial.
-//     embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
-// }
