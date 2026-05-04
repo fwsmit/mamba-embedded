715dc61f3a092ea61a9723ab8302d45723840b7b
Friso Smit <fw.smit01@gmail.com>
2026-05-04 11:16:38 +0700

    Add some performance timimg to the training loop

--- train/train.py
+++ train/train.py
@@ -13,12 +13,14 @@ import os
 from .data import load_har_data, load_mnist_data, load_speechcommands_data
 from .models import TinyMamba,TinyMamba2Multi, TinyMamba3Multi
 from .onnx import export_onnx, test_onnx
+import time
 
 dataset_dir = "./data"
 
 
 def train(model, device, train_loader, optimizer, epoch, print_stats=False, log_interval=10, dry_run=False):
     model.train()
+    _last_print = time.perf_counter()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -27,15 +29,18 @@ def train(model, device, train_loader, optimizer, epoch, print_stats=False, log_
         loss.backward()
         optimizer.step()
         if print_stats and batch_idx % log_interval == 0:
+            _now = time.perf_counter()
             print(
-                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tElapsed: {:.1f}ms".format(
                     epoch,
                     batch_idx * len(data),
                     len(train_loader.dataset),
                     100.0 * batch_idx / len(train_loader),
                     loss.item(),
+                    (_now - _last_print) * 1000,
                 )
             )
+            _last_print = _now
             if dry_run:
                 break
 
