d3a874058590e9135214c0da81a71a13c56e5221
Friso Smit <fw.smit01@gmail.com>
2026-05-23 13:02:05 +0700

    Remove rust project

--- src/data/mod.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(dataset_har)]
-pub mod test_tensor_har;
-
-#[cfg(dataset_kws)]
-pub mod test_tensor_kws;
--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![no_std]
-pub mod models;
-pub use models::mymodel;
-
-pub mod data;
-pub mod utils;
-// use data::mnist_tensor::mnist_tensor as data::mnist_tensor;
-
-// use crate::data::mnist_tensor; ::mnist_tensor;
